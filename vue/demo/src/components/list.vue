6ccde5cf3598cd13c59ce548c7809cc49880537e
tovinping <tovinping@qq.com>
2018-12-30 07:59:19 +0800

    note book

---
+++ vue/demo/src/components/list.vue
@@ -0,0 +1,19 @@
+<template>
+	<ul>
+		<li v-for="item in data" :key="item">{{item}}</li>
+	</ul>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				data: [1,2,3,4,5]
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ vue/demo/src/pages/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{initText}}</h1>
+    <input type="text" v-model="text">
+    <div>你好吗？</div>
+    <hr>
+    <list></list>
+  </div>
+</template>
+<script>
+import List from '@/components/list.vue'
+export default {
+  name: 'home',
+  data(){
+    return {
+      text: ''
+    }
+  },
+  components:{
+    List
+  },
+  computed:{
+    initText(){
+      return this.text || '这是首页'
+    }
+  },
+  mounted(){
+    console.log(this)
+  }
+}
+</script>
+
